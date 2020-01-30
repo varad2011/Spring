3a56ced94a7f8277642a5fc236a26b9d91266856
varad2011 <patilvarad2011@gmail.com>
2020-01-30 09:20:16 +0530

    added spring project

---
+++ springCoreExample/src/main/java/com/springCoreExample/springCoreExample/SpringCoreExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springCoreExample.springCoreExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreExampleApplication.class, args);
+	}
+
+}
---
+++ springCoreExample/src/test/java/com/springCoreExample/springCoreExample/SpringCoreExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springCoreExample.springCoreExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
